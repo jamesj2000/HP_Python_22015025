12cc8b9df8ac2636893685b941c1ce02ec519547
jamesj2000 <134307866+jamesj2000@users.noreply.github.com>
2023-05-22 17:07:27 +0100

    Create python_basic3.py

---
+++ python_basic3.py
@@ -0,0 +1,5 @@
+# Printing into Two Decimal Places
+# Using round() function
+inputNumber = 3.14159
+roundNumber = round(inputNumber, 2)
+print("Rounding 3.14159 upto 2 decimal places: ", round(inputNumbr)
