7f5013fd16b4e63c19c5a85dfda919f35f151bfe
jamesj2000 <134307866+jamesj2000@users.noreply.github.com>
2023-05-22 17:05:52 +0100

    Create python_basic2.py

---
+++ python_basic2.py
@@ -0,0 +1,12 @@
+# Program to swap numbers a and b respectively
+print("Swapper")
+a = 6
+b = 8
+print(f"The value in a = {a}\nThe Value in b = {b}")
+# Swapping from here
+c = a
+a = b
+
+print("\n")
+
+print(f"The value in a = {a}\nThe Value in b = {c}")
